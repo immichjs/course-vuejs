47ba2698a038f79a6cac577c449fb82f5b1685c8
Michel França <fr.mich17@gmail.com>
2021-03-26 23:08:41 -0300

    Módulo Usando Vue CLI concluído

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="#app">
+    {{ title }}
+    <button @click="title += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Teste data usando Vue-cli'
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    background: chocolate;
+    color: #fff;
+  }
+</style>
