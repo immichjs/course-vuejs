db562ba05f0573aeb837e46d1d1fde279cf33311
Michel França <fr.mich17@gmail.com>
2021-04-03 03:26:06 -0300

    Avançando na compreensão sobre comunicações entre componentes

---
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do Usuário: <strong>{{ inverterNome() }}</strong>
+    </p>
+    <button @click="reiniciarNome">Reiniciar Nome</button>
+    <button @click="reiniciarFn()">Reiniciar nome (callback)</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nome: {
+      type: String,
+      default: 'Anônimo',
+      //   required: true,
+      //   default() {
+      //     return Array(10).fill(0).join(',');
+      //   },
+    },
+    reiniciarFn: Function,
+  },
+  methods: {
+    inverterNome() {
+      return this.nome.split('').reverse().join('');
+    },
+    reiniciarNome() {
+      //   const novoNome = 'pedro';
+      this.$emit('nomeMudou', 'Pedro');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
