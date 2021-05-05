f5e0bc4d82248b47538166ecd27122eea3111393
Michel França <fr.mich17@gmail.com>
2021-05-05 19:28:15 -0300

    Aprendendo gerenciamento de estado com Vuex

---
+++ vuex/vuex-exercicios/src/store/modules/carrinho.js
@@ -0,0 +1,30 @@
+export default {
+  namespaced: true,
+  state: {
+    produtos: [],
+  },
+  getters: {
+    valorTotal(state) {
+      return state.produtos.map(prod => prod.quantidade * prod.preco)
+        .reduce((total, atual) => total + atual, 0)
+    }
+  },
+  mutations: {
+    adicionarProduto(state, payload) {
+      state.produtos.push(payload)
+    }
+  },
+  actions: {
+    adicionarProduto({ commit }, payload) {
+      setTimeout(() => commit('adicionarProduto', payload), 1000)
+    }
+    // adicionarProduto: {
+    //   root: true,
+    //   handler({ commit }, payload) {
+    //     setTimeout(() => {
+    //       commit('adicionarProduto', payload)
+    //     }, 500);
+    //   }
+    // }
+  }
+}
