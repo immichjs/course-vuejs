abeddd55520194cbd0a1c16056b679de789d85a8
Michel França <fr.mich17@gmail.com>
2021-07-20 20:59:26 -0300

    calculadora

---
+++ calculadora/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Calculator</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from '@/components/main/Calculator'
+export default {
+  components: { Calculator }
+}
+</script>
+
+<style>
+
+</style>
---
+++ calculadora/src/components/Display.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="display">
+    {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value']
+}
+</script>
+
+<style>
+.display {
+  grid-column: span 4;
+  background-color: #0004;
+  color: #fff;
+  font-family: monospace;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  overflow: hidden;
+  font-size: 2.1rem;
+  padding: 20px;
+}
+</style>
